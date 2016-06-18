6d7c6c805e48484ade033cc78fb69851e03adeb1
Subramanian <vishwanath79@outlook.com>
2016-06-18 10:56:34 -0700

    hacking with swift

---
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Subramanian, Vishwanath on 6/14/16.
+//  Copyright © 2016 Vish. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet var detailImageView: UIImageView!
+
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Whitehouse/Whitehouse/MasterViewController.swift
@@ -0,0 +1,100 @@
+//
+//  MasterViewController.swift
+//  Whitehouse
+//
+//  Created by Subramanian, Vishwanath on 6/10/16.
+//  Copyright © 2016 Vish. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [[String: String]]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        func parseJSON(json: JSON) {
+            for result in json["results"].arrayValue {
+                let title = result["title"].stringValue
+                let body = result["body"].stringValue
+                let sigs = result["signatureCount"].stringValue
+                let obj = ["title": title, "body": body, "sigs": sigs]
+                objects.append(obj)
+            }
+            
+            tableView.reloadData()
+        }
+        
+        
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        if let url = NSURL(string: urlString) {
+            if let data = try? NSData(contentsOfURL: url, options: []){
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    //we're ok to parse
+                    parseJSON(json)
+                }
+            }
+        }
+
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"]
+        return cell
+    }
+
+
+
+
+
+
+}
+
