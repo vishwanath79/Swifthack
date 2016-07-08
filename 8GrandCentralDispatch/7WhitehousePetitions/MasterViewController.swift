29c911f494459e936419c0dd90bd30328e539d1d
Subramanian <vishwanath79@outlook.com>
2016-07-07 23:05:57 -0700

    grand central dispatch

---
+++ 8GrandCentralDispatch/7WhitehousePetitions/MasterViewController.swift
@@ -0,0 +1,152 @@
+//
+//  MasterViewController.swift
+//  7WhitehousePetitions
+//
+//  Created by Subramanian, Vishwanath on 6/27/16.
+//  Copyright © 2016 Vish. All rights reserved.
+//
+
+import UIKit
+
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [[String: String]]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+        let urlString : String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            
+            urlString = "https:// api.whitehouse.gov/ v1/ petitions.json? signatureCountFloor = 10000& limit = 100"
+            
+       
+        }
+        
+        if let url = NSURL(string: urlString) {
+            
+            if let data = try? NSData(contentsOfURL: url, options: []) {
+                
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    
+                    parseJSON(json)
+                } else {
+                    showError()
+                }
+                
+            } else {
+                showError()
+            }
+        } else {
+            showError()
+        }
+        
+    }
+    
+
+
+
+        func showError() {
+            
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+        
+        
+        //self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(insertNewObject(_:)))
+        //self.navigationItem.rightBarButtonItem = addButton
+        //if let split = self.splitViewController {
+         //   let controllers = split.viewControllers
+         //   self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        //}
+
+    
+    
+    
+    func parseJSON(json: JSON) {
+        
+        for result in json["results"].arrayValue {
+            
+            
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title":title, "body":body, "sigs": sigs]
+            
+            objects.append(obj)
+        
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row] 
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"]
+        return cell
+    }
+
+
+
+}
+
+
+
+
+
