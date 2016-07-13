bd436b970c4dcba2ef7e1dcb14ed3b7ef557e904
Subramanian <vishwanath79@outlook.com>
2016-07-12 23:04:16 -0700

    importing photos

--- 10NamestoFaces/10NamestoFaces/ViewController.swift
+++ 10NamestoFaces/10NamestoFaces/ViewController.swift
@@ -8,21 +8,96 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     
-    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(addNewPerson))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) ->
+        Int {
+            
+            return 10
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
+        
+        return cell
+        
+    }
+    
+    func addNewPerson() {
+    
+    let picker = UIImagePickerController()
+        
+    picker.allowsEditing = true
+    
+    picker.delegate = self
+        
+    presentViewController(picker, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func imagePickerController (picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: AnyObject]) {
+        
+        var newImage: UIImage
+        
+        if let possibleImage = info[UIImagePickerControllerEditedImage] as? UiImage {
+            
+            newImage = possibleImage
+        }  else if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            newImage = possibleImage
+        } else {
+            
+            return
+        }
+        
+        let imageName = NSUUID().UUIDString
+        let imagePath = getDocumentsDirectory().stringByAppendingPathComponent(imageName)
+        
+        if let jpegData = UIImageJPEGRepresentation(newImage, 80) {
+            jpegData.writeToFile(imagePath, atomically: true)
+            
+        }
 
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        }
+    
+    func getDocumentsDirectory() -> NSString {
+        
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+        
+    }
+        
+    }
 
 }
 
