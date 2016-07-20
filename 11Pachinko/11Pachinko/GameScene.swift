ec3130e2406d7944e6555b97d8e4bf4e2ba82027
Subramanian <vishwanath79@outlook.com>
2016-07-19 22:07:46 -0700

    spinning slots

--- 11Pachinko/11Pachinko/GameScene.swift
+++ 11Pachinko/11Pachinko/GameScene.swift
@@ -8,7 +8,8 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+    //make the class conform to SKPhysicsContactDelegate property for collision detection
+class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
      let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -18,7 +19,19 @@ class GameScene: SKScene {
         addChild(background)
         // adds a physics body to the whole scene  that is a line on each edge, effectively acting as a container for the scene
         physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
-
+        
+        // Assing the current scene to be the physicss world's contact delegate
+        physicsWorld.contactDelegate = self
+        
+        makeSlotAt(CGPoint(x: 128, y:0), isGood: true)
+        makeSlotAt(CGPoint(x: 384, y:0), isGood: false)
+        makeSlotAt(CGPoint(x: 640, y:0), isGood: true)
+        makeSlotAt(CGPoint(x: 896, y:0), isGood: false)
+        
+        
+        
+        
+        
         makeBouncerAt(CGPoint(x:0, y:0))
         makeBouncerAt(CGPoint(x:256, y:0))
         makeBouncerAt(CGPoint(x:512, y:0))
@@ -42,11 +55,71 @@ class GameScene: SKScene {
             // restitution (bounciness) level of 0.4
             ball.physicsBody!.restitution = 0.4
             ball.position = location
+            
+            ball.name = "ball"
+            
             addChild(ball)
             
         }
         
     
+    }
+    
+    //setting up the scene to be the contact delegate of the physics world
+    //we need to change the contactTestBitMask property of our physics objects which sets the contactDelegate property to be our scene
+
+    
+    
+    
+    
+    // whether the slot is good or not and which image gets loaded
+    func makeSlotAt(position: CGPoint, isGood: Bool) {
+        
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        if isGood {
+            
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            
+            // need to have three names in our code: good slots, bad slots and balls
+            
+            slotBase.name = "good"
+            
+        } else {
+            
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            
+            slotBase.name = "bad"
+        }
+        
+        slotBase.position = position
+        slotGlow.position = position
+        
+        
+        //add rectangle physics to slots
+        slotBase.physicsBody = SKPhysicsBody(rectangleOfSize: slotBase.size)
+        //slotbase needs to be non-dynamic because we dont want it to move out of the way when a player ball hits
+        slotBase.physicsBody!.dynamic = false
+        
+        
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        
+        // angels are specified in radians not degrees. pi radians = 180 degrees
+        //Built in value of pi called M_PI
+        //Have to use CGFLoat with M_PI
+        //WHen you create an action it will run once , if you want it to run forever, you create another action to wrap the first one using repeatActionForever()
+        
+        let spin = SKAction.rotateByAngle(CGFloat(M_PI_2), duration: 10)
+        let spinForever = SKAction.repeatActionForever(spin)
+        slotGlow.runAction(spinForever)
+        
+        
+        
     }
     
     
