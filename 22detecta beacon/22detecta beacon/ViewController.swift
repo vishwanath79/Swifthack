0d5fdd20b4ba42cec6b69e4c5ad6cf27b5fee117
Subramanian <vishwanath79@outlook.com>
2016-10-03 22:10:23 -0700

    CLBeacon Region

--- 22detecta beacon/22detecta beacon/ViewController.swift
+++ 22detecta beacon/22detecta beacon/ViewController.swift
@@ -36,13 +36,58 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 
                 if CLLocationManager.isRangingAvailable() {
-                    //do stuff
+                    startScanning()
                 }
                 
             }
         }
     }
+    
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major:123, minor:456, identifier: "MyBeacon")
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity) {
+        
+        UIView.animate(withDuration: 0.8) { [unowned self] in
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.distanceReading.text = "FAR"
 
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.distanceReading.text = "RIGHT HERE"
+                
+            }
+    }
+
+}
+
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region:CLBeaconRegion) {
+        
+        if beacons.count > 0 {
+            let beacon = beacons[0]
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+        
+    }
+    
 
 }
 
